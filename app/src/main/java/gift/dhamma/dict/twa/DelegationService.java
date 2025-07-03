da8184fe6434aa8321f64096585ce41137bfda91
o7i-wsl <o@yoga.7i>
2025-07-03 08:30:21 -0400

    update from server

--- app/src/main/java/gift/dhamma/dict/twa/DelegationService.java
+++ app/src/main/java/gift/dhamma/dict/twa/DelegationService.java
@@ -1,11 +1,14 @@
 package gift.dhamma.dict.twa;
 
+
+
 public class DelegationService extends
         com.google.androidbrowserhelper.trusted.DelegationService {
-
     @Override
     public void onCreate() {
         super.onCreate();
 
+        
     }
 }
+
